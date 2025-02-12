778207b4269a14370d33aadb26f731131ab5fdb5
Haider Ali <haiderali920687@gmail.com>
2025-02-12 15:12:46 +0500

    Update Curve.jsx

--- frontend/src/components/LandingPage/Curve.jsx
+++ frontend/src/components/LandingPage/Curve.jsx
@@ -17,7 +17,7 @@ export default function Curve() {
                 width:"200px",
                 backgroundColor: "#EFEFEF",
                 borderBottomRightRadius:"20px",
-                marginBottom:"50px",
+                marginBottom:"10px",
                 display:"flex",
                 justifyContent:"center",
                 alignItems:'center',
